b3087b53007cfa5fd7212ee27312774befe91d5b
zoejingwen <jshen9613@gmail.com>
2025-11-10 10:36:58 +0100

    resnet18

--- prepareData.m
+++ prepareData.m
@@ -1,19 +1,15 @@
 clear all
 close all
 
-folderName = "food-11\train"
-
+folderName = "C:\Users\moqian\Desktop\food-11\train"
 imds = imageDatastore(folderName, ...
-    IncludeSubfolders=true, ...
-    LabelSource="foldernames");
-
+    'IncludeSubfolders', true, ...
+    'LabelSource', "foldernames");
+[imdsTrain,imdsValidation] = splitEachLabel(imds, 0.7, "randomized");
 numImages = numel(imds.Labels);
 idx = randperm(numImages,16);
-I = imtile(imds,Frames=idx);
+I = imtile(imds, 'Frames', idx);
 figure
 imshow(I)
-
 classNames = categories(imds.Labels);
 numClasses = numel(classNames);
-
-[imdsTrain,imdsValidation] = splitEachLabel(imds,0.7,0.3,"randomized");
